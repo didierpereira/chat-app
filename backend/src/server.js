276660f8ffbd899cd83a9f42f4e70f33286b7028
Didier Pereira <krrattoss5@gmail.com>
2026-02-06 23:41:43 -0500

    signup setup

---
+++ backend/src/server.js
@@ -0,0 +1,20 @@
+import express from "express"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.routes.js"
+import { connectDB } from "./lib/db.js"
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+app.use("/api/auth", authRoutes)
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+  connectDB()
+})
+
