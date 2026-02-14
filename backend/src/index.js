46b81abd6f016c59bc4fb9049651f0d5378cc8de
Didier Pereira <krrattoss5@gmail.com>
2026-02-13 21:04:48 -0500

    fix try production route assignement

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,43 +1,43 @@
-import cookieParser from "cookie-parser"
-import cors from "cors"
-import dotenv from "dotenv"
-import express from "express"
-import { connectDB } from "./lib/db.js"
-import authRoutes from "./routes/auth.routes.js"
-import messageRoutes from "./routes/message.routes.js"
-import path from "path"
+  import cookieParser from "cookie-parser"
+  import cors from "cors"
+  import dotenv from "dotenv"
+  import express from "express"
+  import { connectDB } from "./lib/db.js"
+  import authRoutes from "./routes/auth.routes.js"
+  import messageRoutes from "./routes/message.routes.js"
+  import path from "path"
 
-import { io, server, app } from "./lib/socket.js"
+  import { io, server, app } from "./lib/socket.js"
 
-const __dirname = path.resolve()
+  const __dirname = path.resolve()
 
-dotenv.config()
+  dotenv.config()
 
-// aumentar el límite para payloads grandes (imágenes en base64)
-app.use(express.json({ limit: "10mb" }))
-app.use(cookieParser())
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   }),
-// )
+  // aumentar el límite para payloads grandes (imágenes en base64)
+  app.use(express.json({ limit: "10mb" }))
+  app.use(cookieParser())
+  // app.use(
+  //   cors({
+  //     origin: "http://localhost:5173",
+  //     credentials: true,
+  //   }),
+  // )
 
-app.use("/api/auth", authRoutes)
+  app.use("/api/auth", authRoutes)
 
-app.use("/api/messages", messageRoutes)
+  app.use("/api/messages", messageRoutes)
 
-const PORT = process.env.PORT || 5001
+  const PORT = process.env.PORT || 5001
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get(/^(?!\/api).+/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../../frontend", "dist", "index.html"))
+    app.get(/^(?!\/api).+/, (req, res) => {
+      res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+    })
+  }
+
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+    connectDB()
   })
-}
-
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-  connectDB()
-})
