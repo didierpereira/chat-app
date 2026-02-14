15f3444472ec84a6802719f62cb873bb7b935444
Didier Pereira <krrattoss5@gmail.com>
2026-02-13 20:42:33 -0500

    fix production route assignement

--- backend/src/index.js
+++ backend/src/index.js
@@ -32,8 +32,8 @@ const PORT = process.env.PORT || 5001
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get("(.*)", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
   })
 }
 
